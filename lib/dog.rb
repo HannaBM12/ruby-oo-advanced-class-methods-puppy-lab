378e577d8da5d59b4be4ebc3a0453469997fb797
HannaBM12 <Hannabmulugeta@gmail.com>
2021-03-08 01:05:40 -0500

    commit tree 65cb3cfbe83000b07557acebbb4ea6aa4fcd48b9

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,8 @@
-# Add your code here
-require 'pry'
 class Dog
     attr_reader :name
+
     @@all = []
-    def initialize (name)
+    def initialize(name)
         @name = name
         save
     end
@@ -13,15 +12,88 @@ class Dog
     end
 
     def self.clear_all
-        @@all.clear()
+        self.all.clear
     end
 
     def self.print_all
-        @@all.map{|k| puts k.name }
+        self.all.each{|dog| puts dog.name}
     end
 
     def save
         @@all << self
     end
-    
 end
+
+
+
+
+
+# class Dog
+#     attr_reader :name
+#     @@all = []
+
+#     def initialize(name)
+#         @name = name
+#         save
+#     end
+
+#     def save
+#         @@all << self
+#     end
+
+#     def self.all
+#         @@all
+#     end
+
+#     def self.print_all
+#         self.all.each{|dog| puts "#{dog.name}"}
+#     end
+
+#     def self.clear_all
+#         @@all.clear
+#     end
+
+    
+    
+
+
+
+# end
+
+
+
+
+
+
+
+
+
+
+
+# # Add your code here
+# require 'pry'
+# class Dog
+#     attr_reader :name
+#     @@all = []
+#     def initialize (name)
+#         @name = name
+#         save
+#     end
+
+#     def self.all
+#         @@all
+#     end
+
+#     def self.clear_all
+#         @@all.clear()
+#     end
+
+#     def self.print_all
+#         @@all.map{|k| puts k.name }
+#     end
+
+#     def save
+#         @@all << self
+#     end
+    
+# end
